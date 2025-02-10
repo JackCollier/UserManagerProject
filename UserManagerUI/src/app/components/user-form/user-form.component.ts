494ed09bd0c29d8aa8cedcb9ee9e63cf0199de33
JacvaSipt <121406496+JacvaSipt@users.noreply.github.com>
2025-02-10 20:50:41 +0000

    create form to handle adding a user

--- UserManagerUI/src/app/components/user-form/user-form.component.ts
+++ UserManagerUI/src/app/components/user-form/user-form.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-user-form',
-  imports: [],
   templateUrl: './user-form.component.html',
+  imports: [ReactiveFormsModule]
 })
 export class UserFormComponent {
+  userForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private userService: UserService) {
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      role: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.userForm.valid) {
+      const newUser: User = {
+        id: 0, 
+        name: this.userForm.value.name,
+        email: this.userForm.value.email,
+        role: this.userForm.value.role
+      };
+
+      this.userService.addUser(newUser).subscribe(() => {
+        this.userForm.reset();
+      });
+    }
+  }
 }
