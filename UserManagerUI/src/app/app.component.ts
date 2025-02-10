c6e0c7f11b2cd6538be4c2cdaf6b24687f8ea00e
JacvaSipt <121406496+JacvaSipt@users.noreply.github.com>
2025-02-10 20:39:36 +0000

    update app to show user list

--- UserManagerUI/src/app/app.component.ts
+++ UserManagerUI/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';;
+import { UserListComponent } from "./components/user-list/user-list.component";
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [UserListComponent]
 })
 export class AppComponent {
   title = 'UserManagerUI';
