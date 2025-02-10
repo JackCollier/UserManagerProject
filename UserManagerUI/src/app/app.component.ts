b2745000268fd9662f1b32fe57010c593950ad34
JacvaSipt <121406496+JacvaSipt@users.noreply.github.com>
2025-02-10 20:50:51 +0000

    update app to show form

--- UserManagerUI/src/app/app.component.ts
+++ UserManagerUI/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';;
 import { UserListComponent } from "./components/user-list/user-list.component";
 import { HttpClientModule } from '@angular/common/http';
+import { UserFormComponent } from "./components/user-form/user-form.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [UserListComponent]
+  imports: [UserListComponent, UserFormComponent]
 })
 export class AppComponent {
   title = 'UserManagerUI';
