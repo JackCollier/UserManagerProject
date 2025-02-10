634ce9f0db5684e99f12ebb100e02c5b6c791215
JacvaSipt <121406496+JacvaSipt@users.noreply.github.com>
2025-02-10 20:25:59 +0000

    add cors to back end

--- ManageUserAPI/Program.cs
+++ ManageUserAPI/Program.cs
@@ -6,6 +6,17 @@ builder.Services.AddControllers().AddNewtonsoftJson();
 
 builder.Services.AddScoped<UserService>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -13,6 +24,7 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 
+app.UseCors("AllowAngularApp"); 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
