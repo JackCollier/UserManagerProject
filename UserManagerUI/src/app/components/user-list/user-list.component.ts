b68262f879f2394430d60cada7c698fb05d9d0ed
JacvaSipt <121406496+JacvaSipt@users.noreply.github.com>
2025-02-10 20:39:19 +0000

    display and delete users from list

--- UserManagerUI/src/app/components/user-list/user-list.component.ts
+++ UserManagerUI/src/app/components/user-list/user-list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { User } from '../../models/user';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-user-list',
-  imports: [],
   templateUrl: './user-list.component.html',
+  imports: [CommonModule]
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+  users: User[] = [];
 
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe(users => {
+      this.users = users;
+    });
+  }
+
+  deleteUser(id: number): void {
+    this.userService.deleteUser(id).subscribe(() => {
+      this.users = this.users.filter(user => user.id !== id);
+    });
+  }
 }
