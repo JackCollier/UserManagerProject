8e9fe72afb8c9d8bc0387c52c7ededb5878d8d0a
JacvaSipt <121406496+JacvaSipt@users.noreply.github.com>
2025-02-11 16:23:46 +0000

    enable user editing in form component

--- UserManagerUI/src/app/components/user-form/user-form.component.ts
+++ UserManagerUI/src/app/components/user-form/user-form.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UserService } from '../../services/user.service';
 import { User } from '../../models/user';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
-  imports: [ReactiveFormsModule]
+  imports: [ReactiveFormsModule, CommonModule]
 })
 export class UserFormComponent {
   userForm: FormGroup;
+  isEditing = false;
+  editingUserId: number | null = null;
+
+  @Input() set selectedUser(user: User | null) {
+    if (user) {
+      this.isEditing = true;
+      this.editingUserId = user.id;
+      this.userForm.patchValue(user); 
+    } else {
+      this.isEditing = false;
+      this.editingUserId = null;
+      this.userForm.reset();
+    }
+  }
 
   constructor(private fb: FormBuilder, private userService: UserService) {
     this.userForm = this.fb.group({
@@ -21,16 +36,28 @@ export class UserFormComponent {
 
   onSubmit(): void {
     if (this.userForm.valid) {
-      const newUser: User = {
-        id: 0, 
+      const user: User = {
+        id: this.editingUserId || 0,
         name: this.userForm.value.name,
         email: this.userForm.value.email,
         role: this.userForm.value.role
       };
 
-      this.userService.addUser(newUser).subscribe(() => {
-        this.userForm.reset();
-      });
+      if (this.isEditing) {
+        this.userService.updateUser(user).subscribe(() => {
+          this.cancelEdit();
+        });
+      } else {
+        this.userService.addUser(user).subscribe(() => {
+          this.userForm.reset();
+        });
+      }
     }
+  }
+
+  cancelEdit(): void {
+    this.isEditing = false;
+    this.editingUserId = null;
+    this.userForm.reset();
   }
 }
