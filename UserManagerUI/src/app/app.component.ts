61d1200ffe7ceb8785cc408036c106406f9bca2e
JacvaSipt <121406496+JacvaSipt@users.noreply.github.com>
2025-02-11 16:24:31 +0000

    alllow editing user data to be passed between components

--- UserManagerUI/src/app/app.component.ts
+++ UserManagerUI/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';;
 import { UserListComponent } from "./components/user-list/user-list.component";
 import { HttpClientModule } from '@angular/common/http';
 import { UserFormComponent } from "./components/user-form/user-form.component";
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,9 @@ import { UserFormComponent } from "./components/user-form/user-form.component";
 })
 export class AppComponent {
   title = 'UserManagerUI';
+  editingUser: User | null = null;
+
+  onUserSelected(user: User): void {
+    this.editingUser = user;
+  }
 }
